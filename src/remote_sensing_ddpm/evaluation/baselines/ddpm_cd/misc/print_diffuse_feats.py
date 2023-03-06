8d427d8b482e88bd1a45b1c862331be4e668309d
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-03-06 11:52:46 +0100

    Added CD training loop

--- src/remote_sensing_ddpm/evaluation/baselines/ddpm_cd/misc/print_diffuse_feats.py
+++ src/remote_sensing_ddpm/evaluation/baselines/ddpm_cd/misc/print_diffuse_feats.py
@@ -1,4 +1,4 @@
-import core.metrics as Metrics
+import remote_sensing_ddpm.evaluation.baselines.ddpm_cd.core.metrics as Metrics
 import os
 
 
@@ -6,11 +6,16 @@ def print_feats(opt, train_data, feats_A, feats_B, level, t):
     feature_path = "{}/features".format(opt["path"]["results"])
     os.makedirs(feature_path, exist_ok=True)
 
-    img_A = Metrics.tensor2img(train_data["A"][0, :, :, :])  # uint8
-    Metrics.save_img(img_A, "{}/img_A.png".format(feature_path))
+    if feats_B:
+        a_key = "A"
+    else:
+        a_key = "image"
 
-    img_B = Metrics.tensor2img(train_data["B"][0, :, :, :])  # uint8
-    Metrics.save_img(img_B, "{}/img_B.png".format(feature_path))
+    img_A = Metrics.tensor2img(train_data[a_key][0, :, :, :])  # uint8
+    Metrics.save_img(img_A, "{}/img_A.png".format(feature_path))
+    if feats_B:
+        img_B = Metrics.tensor2img(train_data["B"][0, :, :, :])  # uint8
+        Metrics.save_img(img_B, "{}/img_B.png".format(feature_path))
 
     for i in range(feats_A[level].size(1)):
         feat_img_A = Metrics.tensor2img(feats_A[level][0, i, :, :])  # uint8
@@ -19,8 +24,9 @@ def print_feats(opt, train_data, feats_A, feats_B, level, t):
             "{}/feat_A_{}_level_{}_t_{}.png".format(feature_path, i, level, t),
         )
 
-        feat_img_B = Metrics.tensor2img(feats_B[level][0, i, :, :])  # uint8
-        Metrics.save_feat(
-            feat_img_B,
-            "{}/feat_B_{}_level_{}_t_{}.png".format(feature_path, i, level, t),
-        )
+        if feats_B:
+            feat_img_B = Metrics.tensor2img(feats_B[level][0, i, :, :])  # uint8
+            Metrics.save_feat(
+                feat_img_B,
+                "{}/feat_B_{}_level_{}_t_{}.png".format(feature_path, i, level, t),
+            )
