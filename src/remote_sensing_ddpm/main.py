ddff42159f0f6ccd9b7ad326fa91b20b2d6e3b3e
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-03-10 10:05:06 +0100

    Added TODOs to main.py

--- src/remote_sensing_ddpm/main.py
+++ src/remote_sensing_ddpm/main.py
@@ -1,24 +1,9 @@
-import json
-import torch
-from remote_sensing_ddpm.evaluation.baselines.ddpm_cd import (
-    create_model,
-    create_CD_model,
-    create_classification_model,
-)
 
 
-class NoneDict(dict):
-    def __getitem__(self, key):
-        return dict.get(self, key)
+# TODO load ben-ge dataset
 
+# TODO load U-Net model
 
-if __name__ == "__main__":
-    with open(
-        "/Users/alexanderlontke/Documents/Uni/St. Gallen/SS_23/master_thesis/remote-sensing-ddpm/config/baselines/ddpm-cd/classification.json",
-        "rb",
-    ) as config_file:
-        opt = NoneDict(json.load(config_file))
-    model = create_classification_model(opt)
-    print(model.print_network())
+# TODO Instantiate DDPM class
 
-    model.classification_net(torch.rand((256, 256, 3)))
+# TODO code training loop
