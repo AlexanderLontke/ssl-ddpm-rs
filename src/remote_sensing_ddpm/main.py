a56d1f1250d0d501193a81727a23a97967187dc2
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-03-29 15:29:21 +0200

    Added training setup and config

--- src/remote_sensing_ddpm/main.py
+++ src/remote_sensing_ddpm/main.py
@@ -1,9 +1,64 @@
+from typing import Dict
+
+# PyTorch imports
+from torch.utils.data import DataLoader
+
+# PyTorch Lightning
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelSummary, ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+
+# Util
+from remote_sensing_ddpm.util import instantiate_python_class_from_string_config
+from remote_sensing_ddpm.constants import (
+    TORCH_DATA_LOADER_CONFIG_KEY,
+    P_THETA_MODEL_CONFIG_KEY,
+    DDPM_CONFIG_KEY,
+    PL_TRAINER_CONFIG_KEY,
+    PL_WANDB_LOGGER_CONFIG_KEY,
+    PL_MODEL_CHECKPOINT_CONFIG_KEY
+)
+# Diffusion
+from remote_sensing_ddpm.diffusion_process.ddpm import LitDDPM
 
 
-# TODO load ben-ge dataset
+def main(config: Dict):
+    # Instantiate dataset
+    train_dataset = ...
+    # Instantiate dataloader from data set
+    train_data_loader = DataLoader(
+        dataset=train_dataset,
+        **config[TORCH_DATA_LOADER_CONFIG_KEY]
+    )
+    # Instantiate model approximating p_theta(x_{t-1}|x_t)
+    p_theta_model = instantiate_python_class_from_string_config(
+        class_config=config[P_THETA_MODEL_CONFIG_KEY]
+    )
+    # TODO Instantiate DDPM class
+    ddpm_pl_module = LitDDPM(
+        p_theta_model=p_theta_model,
+        **config[DDPM_CONFIG_KEY]
+    )
 
-# TODO load U-Net model
-
-# TODO Instantiate DDPM class
-
-# TODO code training loop
+    # PL-Trainer with the following features:
+    # - Model Summary
+    # - Progressbar
+    # - WAND logging
+    # - Checkpointing
+    trainer = pl.Trainer(
+        **config[PL_TRAINER_CONFIG_KEY],
+        logger=WandbLogger(
+            **config[PL_WANDB_LOGGER_CONFIG_KEY]
+        ),
+        callbacks=[
+            ModelSummary(),
+            ModelCheckpoint(
+                **config[PL_MODEL_CHECKPOINT_CONFIG_KEY],
+            ),
+        ]
+    )
+    # Run training
+    trainer.fit(
+        model=ddpm_pl_module,
+        train_dataloaders=train_data_loader,
+    )
---
+++ src/remote_sensing_ddpm/util.py
@@ -0,0 +1,21 @@
+from typing import Dict
+
+from importlib import import_module
+
+from remote_sensing_ddpm.constants import PYTHON_CLASS_CONFIG_KEY, STRING_PARAMS_CONFIG_KEY
+
+
+def instantiate_python_class_from_string_config(class_config: Dict):
+    # Assert that necessary keys are contained in config
+    assert all(k in class_config.keys() for k in [PYTHON_CLASS_CONFIG_KEY, STRING_PARAMS_CONFIG_KEY])
+    # Get module and class names
+    module_full_name: str = class_config[PYTHON_CLASS_CONFIG_KEY]
+    module_sub_names = module_full_name.split(".")
+    module_name = ".".join(module_sub_names[:-1])
+    class_name = module_sub_names[-1]
+    # Import necessary module
+    module = import_module(module_name)
+    # Instantiate class with config values
+    return module.__getattr__(class_name)(
+        **class_config[STRING_PARAMS_CONFIG_KEY]
+    )
