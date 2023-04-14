41b0ab7eed039cac802e11add6112281ab948044
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-04-14 16:11:51 +0200

    Reworked classification_downstream_task.py

--- src/remote_sensing_ddpm/downstream_tasks/downstream_models/classification_downstream_task.py
+++ src/remote_sensing_ddpm/downstream_tasks/downstream_models/classification_downstream_task.py
@@ -7,8 +7,8 @@ from remote_sensing_ddpm.downstream_tasks.downstream_models.downstream_task_mode
 
 class ClassificationDownstreamTask(DownstreamTaskModel):
     def __init__(self, input_size: int, output_size: int, *args, **kwargs):
-        downstream_layer = nn.Sequential([
+        downstream_layer = nn.Sequential(
                 nn.Linear(in_features=input_size, out_features=output_size,),
-                nn.BatchNorm1d()
-        ])
+                nn.BatchNorm1d(num_features=output_size, eps=1e-5,),
+        )
         super().__init__(downstream_layer=downstream_layer, *args, **kwargs)
