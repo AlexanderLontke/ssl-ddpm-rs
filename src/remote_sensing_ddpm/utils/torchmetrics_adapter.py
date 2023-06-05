faecc647aa5b7b95d7f00375aa3310b9e975b5d2
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-06-05 12:06:59 +0200

    Updated torchmetrics_adapter.py

--- src/remote_sensing_ddpm/utils/torchmetrics_adapter.py
+++ src/remote_sensing_ddpm/utils/torchmetrics_adapter.py
@@ -1,27 +1,34 @@
+from typing import Any
+
 import torch
-from torch import nn
 
 from torchmetrics.metric import Metric
 
 
-class TorchmetricsAdapter(nn.Module):
+class TorchmetricsAdapter(Metric):
     def __init__(
         self,
         torchmetrics_module: Metric,
         apply_argmax: bool,
         device: str,
-        *args,
-        **kwargs
     ):
-        super().__init__(*args, **kwargs)
-        self.device = torch.device(device)
-        self.torchmetrics_module = torchmetrics_module.to(self.device)
+        super().__init__()
+        self.torchmetrics_module = torchmetrics_module.to(torch.device(device))
         self.apply_argmax = apply_argmax
 
-    def forward(self, inputs: torch.Tensor, targets: torch.Tensor):
-        inputs = inputs.to(self.device)
-        targets = targets.to(self.device)
+    def _format_input(self, inputs: torch.Tensor, targets: torch.Tensor):
+        inputs = inputs.to(self.torchmetrics_module.device)
+        targets = targets.to(self.torchmetrics_module.device)
 
         if self.apply_argmax:
             inputs = torch.argmax(inputs, dim=1)
-        return self.torchmetrics_module(inputs, targets)
+        return inputs, targets
+
+    def update(self, inputs: torch.Tensor, targets: torch.Tensor) -> None:
+        inputs, targets = self._format_input(
+            inputs=inputs, targets=targets
+        )
+        return self.torchmetrics_module.update(inputs, targets)
+
+    def compute(self) -> Any:
+        return self.torchmetrics_module.compute()
