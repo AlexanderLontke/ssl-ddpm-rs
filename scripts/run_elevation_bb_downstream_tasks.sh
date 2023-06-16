6f294c182295fcc51af3f6224d47583f1c697be7
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-06-16 13:25:07 +0200

    Bulk commit

---
+++ scripts/run_elevation_bb_downstream_tasks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+BACKBONE_CONFIG=$1
+
+# Run all six downstream tasks one after the other
+python src/remote_sensing_ddpm/train_downstream_tasks.py -b $BACKBONE_CONFIG -d config/model_configs/downstream_tasks/ewc-classification.yaml
+python src/remote_sensing_ddpm/train_downstream_tasks.py -b $BACKBONE_CONFIG -d config/model_configs/downstream_tasks/climate-zone-classification.yaml
+python src/remote_sensing_ddpm/train_downstream_tasks.py -b $BACKBONE_CONFIG -d config/model_configs/downstream_tasks/ewc-regression.yaml
+python src/remote_sensing_ddpm/train_downstream_tasks.py -b $BACKBONE_CONFIG -d config/model_configs/downstream_tasks/ewc-segmentation.yaml
