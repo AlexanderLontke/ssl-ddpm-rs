7a5f70b217d16d3ba9e3629eb584e0c5b66cda06
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-06-24 16:00:07 +0200

    Improved segmentation head

--- src/remote_sensing_ddpm/downstream_tasks/models/non_linear_segmentation_head.py
+++ src/remote_sensing_ddpm/downstream_tasks/models/non_linear_segmentation_head.py
@@ -22,8 +22,11 @@ class NonLinearSegmentationHead(nn.Module):
         self.second_level_decoder = UpSamplingBlock(
             in_channels=512, out_channels=128, kernel_size=2, stride=2,  # 64x64
         )
+        self.first_level_decoder = nn.Conv2d(
+            in_channels=256, out_channels=128, padding=1, kernel_size=3,  # 128x128
+        )
         self.output_projection = nn.Conv2d(
-            in_channels=256, out_channels=output_classes, padding=1, kernel_size=3,  # 128x128
+            in_channels=128, out_channels=output_classes, padding=0, kernel_size=9,
         )
 
     def forward(self, all_decoder_levels: torch.Tensor, ):
@@ -31,16 +34,13 @@ class NonLinearSegmentationHead(nn.Module):
         bottle_neck, fourth_level, third_level, second_level, first_level = all_decoder_levels
         # Bottleneck: shape [1024, 8, 8]
         x = torch.cat([self.bottleneck_decoder(bottle_neck),  fourth_level], dim=1)
-        print("Shape 1", x.shape)
         # Fourth level decoder: shape [1024, 16, 16]
         x = torch.cat([self.fourth_level_decoder(x), third_level], dim=1)
-        print("Shape 2", x.shape)
         # Third level decoder: shape [512, 32, 32]
         x = torch.cat([self.third_level_decoder(x), second_level], dim=1)
-        print("Shape 3", x.shape)
         # Second level decoder: shape [256, 64, 64]
         x = torch.cat([self.second_level_decoder(x), first_level], dim=1)
-        print("Shape 4", x.shape)
         # First level decoder: shape [128, 128, 128]
+        x = self.first_level_decoder(x)
         # Project into target dimensions
         return self.output_projection(x)
--- src/remote_sensing_ddpm/downstream_tasks/modules/up_sampling_block.py
+++ src/remote_sensing_ddpm/downstream_tasks/modules/up_sampling_block.py
@@ -4,11 +4,19 @@ from torch import nn
 class UpSamplingBlock(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, kernel_size:int, stride:int):
         super().__init__()
-        self.conv = nn.ConvTranspose2d(
+        self.up_conv = nn.ConvTranspose2d(
             in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride,
         )
-        self.batch_norm = nn.BatchNorm2d(num_features=out_channels)
+        self.conv1 = nn.Conv2d(
+            in_channels=out_channels, out_channels=out_channels, padding=1, kernel_size=3,
+        )
+        self.batch_norm1 = nn.BatchNorm2d(num_features=out_channels)
+        self.conv2 = nn.Conv2d(
+            in_channels=out_channels, out_channels=out_channels, padding=1, kernel_size=3,
+        )
+        self.batch_norm2 = nn.BatchNorm2d(num_features=out_channels)
         self.relu = nn.ReLU()
 
     def forward(self, batch):
-        return self.relu(self.batch_norm(self.conv(batch)))
+        x = self.relu(self.batch_norm1(self.conv1(self.up_conv(batch))))
+        return self.relu(self.batch_norm2(self.conv2(x)))
