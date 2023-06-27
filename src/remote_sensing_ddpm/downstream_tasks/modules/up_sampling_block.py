6c799cb2409466a1ce8de6386bd505995a3d7e87
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-06-27 18:13:18 +0200

    Slimmed down non linear segmentation head

--- src/remote_sensing_ddpm/downstream_tasks/modules/up_sampling_block.py
+++ src/remote_sensing_ddpm/downstream_tasks/modules/up_sampling_block.py
@@ -7,16 +7,17 @@ class UpSamplingBlock(nn.Module):
         self.up_conv = nn.ConvTranspose2d(
             in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride,
         )
-        self.conv1 = nn.Conv2d(
-            in_channels=out_channels, out_channels=out_channels, padding=1, kernel_size=3,
-        )
-        self.batch_norm1 = nn.BatchNorm2d(num_features=out_channels)
-        self.conv2 = nn.Conv2d(
-            in_channels=out_channels, out_channels=out_channels, padding=1, kernel_size=3,
-        )
-        self.batch_norm2 = nn.BatchNorm2d(num_features=out_channels)
+        # self.conv1 = nn.Conv2d(
+        #     in_channels=out_channels, out_channels=out_channels, padding=1, kernel_size=3,
+        # )
+        # self.batch_norm1 = nn.BatchNorm2d(num_features=out_channels)
+        # self.conv2 = nn.Conv2d(
+        #     in_channels=out_channels, out_channels=out_channels, padding=1, kernel_size=3,
+        # )
+        # self.batch_norm2 = nn.BatchNorm2d(num_features=out_channels)
         self.relu = nn.ReLU()
 
     def forward(self, batch):
-        x = self.relu(self.batch_norm1(self.conv1(self.up_conv(batch))))
-        return self.relu(self.batch_norm2(self.conv2(x)))
+        # x = self.relu(self.batch_norm1(self.conv1(self.up_conv(batch))))
+        # return self.relu(self.batch_norm2(self.conv2(x)))
+        return self.relu(self.up_conv(batch))
