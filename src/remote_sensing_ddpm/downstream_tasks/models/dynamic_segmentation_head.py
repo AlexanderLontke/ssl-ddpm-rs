97e22df2fc98972ca0148714766994b54c1ad371
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-07-23 18:29:18 +0200

    Fixed module list

--- src/remote_sensing_ddpm/downstream_tasks/models/dynamic_segmentation_head.py
+++ src/remote_sensing_ddpm/downstream_tasks/models/dynamic_segmentation_head.py
@@ -10,9 +10,9 @@ class DynamicSegmentationHead(nn.Module):
     def __init__(self,output_channels: int,  u_net_channels: List[int], *args, **kwargs):
         super().__init__(*args, **kwargs)
         assert len(u_net_channels) > 0
-        self.modules = []
+        modules = []
         for i in range(len(u_net_channels)-1):
-            self.modules.append(
+            modules.append(
                 UpSamplingBlock(
                     in_channels=u_net_channels[i] * (1 + int(i != 0)),
                     out_channels=u_net_channels[i+1],
@@ -20,15 +20,16 @@ class DynamicSegmentationHead(nn.Module):
                     stride=2,
                 )
             )
+        self.up_convolutions = nn.ModuleList(modules=modules)
         self.output_projection = nn.Conv2d(
             in_channels=u_net_channels[-1]*2, out_channels=output_channels, padding=0, kernel_size=9,
         )
 
-    def forward(self, all_decoder_levels: torch.Tensor, ):
+    def forward(self, all_decoder_levels: torch.Tensor):
         assert len(all_decoder_levels) == 5, f"Expected five feature maps but got {len(all_decoder_levels)}"
 
         x = all_decoder_levels[0]
-        for i, module in enumerate(self.modules):
+        for i, module in enumerate(self.up_convolutions):
             x = torch.cat([module(x), all_decoder_levels[i+1]], dim=1)
 
         # Project into target dimensions
