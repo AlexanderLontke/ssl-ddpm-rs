09978c49f6a997cebf5b7781fd38e3313f431078
AlexanderLontke <77338841+AlexanderLontke@users.noreply.github.com>
2023-09-23 21:30:52 +0200

    Added missing supervised experiment setups

--- scripts/run_dfc_experiments.sh
+++ scripts/run_dfc_experiments.sh
@@ -13,7 +13,12 @@ python src/remote_sensing_ddpm/run_downstream_tasks.py -b config/model_configs/d
 python src/remote_sensing_ddpm/run_downstream_tasks.py -b config/model_configs/downstream_tasks/dfc_2020/classification/dfc_2020_feature_extractor-lf-50.yaml -d config/model_configs/downstream_tasks/dfc_2020/classification/classification.yaml -r 1
 
 
-# # Segmentations
+# Segmentations
 python src/remote_sensing_ddpm/run_downstream_tasks.py -b config/model_configs/downstream_tasks/dfc_2020/segmentation/dfc_2020_feature_extractor-lf-1.yaml -d config/model_configs/downstream_tasks/dfc_2020/segmentation/segmentation.yaml -r 1
 python src/remote_sensing_ddpm/run_downstream_tasks.py -b config/model_configs/downstream_tasks/dfc_2020/segmentation/dfc_2020_feature_extractor-lf-10.yaml -d config/model_configs/downstream_tasks/dfc_2020/segmentation/segmentation.yaml -r 1
 python src/remote_sensing_ddpm/run_downstream_tasks.py -b config/model_configs/downstream_tasks/dfc_2020/segmentation/dfc_2020_feature_extractor-lf-50.yaml -d config/model_configs/downstream_tasks/dfc_2020/segmentation/segmentation.yaml -r 1
+
+# Regression
+python src/remote_sensing_ddpm/run_downstream_tasks.py -b config/model_configs/downstream_tasks/dfc_2020/regression/dfc_2020_feature_extractor-lf-1.yaml -d config/model_configs/downstream_tasks/dfc_2020/regression/regression.yaml -r 1
+python src/remote_sensing_ddpm/run_downstream_tasks.py -b config/model_configs/downstream_tasks/dfc_2020/regression/dfc_2020_feature_extractor-lf-10.yaml -d config/model_configs/downstream_tasks/dfc_2020/regression/regression.yaml -r 1
+python src/remote_sensing_ddpm/run_downstream_tasks.py -b config/model_configs/downstream_tasks/dfc_2020/regression/dfc_2020_feature_extractor-lf-50.yaml -d config/model_configs/downstream_tasks/dfc_2020/regression/regression.yaml -r 1
--- scripts/run_supervised_lf_baselines.sh
+++ scripts/run_supervised_lf_baselines.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Segmentation
-python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fractions/supervised-dfc2020-segmentation-lf-1.yaml
-python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fractions/supervised-dfc2020-segmentation-lf-10.yaml
-python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fractions/supervised-dfc2020-segmentation-lf-50.yaml
+# python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fractions/supervised-dfc2020-segmentation-lf-1.yaml
+# python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fractions/supervised-dfc2020-segmentation-lf-10.yaml
+# python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fractions/supervised-dfc2020-segmentation-lf-50.yaml
 
 # Classification
-python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fractions/supervised-dfc2020-classification-lf-1.yaml
+# python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fractions/supervised-dfc2020-classification-lf-1.yaml
 python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fractions/supervised-dfc2020-classification-lf-10.yaml
 python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fractions/supervised-dfc2020-classification-lf-50.yaml
 
@@ -14,3 +14,24 @@ python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fra
 python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fractions/supervised-dfc2020-regression-lf-1.yaml
 python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fractions/supervised-dfc2020-regression-lf-10.yaml
 python -m lit_diffusion.train -c config/baselines/supervised-baselines/label-fractions/supervised-dfc2020-regression-lf-50.yaml
+
+# Full Runs
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/segmentation/supervised-dfc2020-segmentation-seed-42.yaml
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/classification/supervised-dfc2020-classification-seed-42.yaml
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/regression/supervised-dfc2020-regression-seed-42.yaml
+
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/segmentation/supervised-dfc2020-segmentation-seed-43.yaml
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/classification/supervised-dfc2020-classification-seed-43.yaml
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/regression/supervised-dfc2020-regression-seed-43.yaml
+
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/segmentation/supervised-dfc2020-segmentation-seed-44.yaml
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/classification/supervised-dfc2020-classification-seed-44.yaml
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/regression/supervised-dfc2020-regression-seed-44.yaml
+
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/segmentation/supervised-dfc2020-segmentation-seed-45.yaml
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/classification/supervised-dfc2020-classification-seed-45.yaml
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/regression/supervised-dfc2020-regression-seed-45.yaml
+
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/segmentation/supervised-dfc2020-segmentation-seed-46.yaml
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/classification/supervised-dfc2020-classification-seed-46.yaml
+python -m lit_diffusion.train -c config/baselines/supervised-baselines/regression/supervised-dfc2020-regression-seed-46.yaml
